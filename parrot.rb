f99cff93f38da72d5c32d7b3e4f9a8172280e140
Matt Mason <masonmj10@gmail.com>
2020-07-07 20:06:49 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot(sound = "Squawk!")
+  puts "A bird goes #{sound}."
+  return parrot
+end
+parrot("tweet tweet")
